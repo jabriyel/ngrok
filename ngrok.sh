bb8214a2a1f85369f4ff06674eb2d8e21e1aec0f
jabriyel <53429135+jabriyel@users.noreply.github.com>
2024-04-25 22:21:54 +0100

    Update ngrok.sh
    update 1

--- ngrok.sh
+++ ngrok.sh
@@ -9,16 +9,28 @@ echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/
 # Update package index
 sudo apt update
 
-ngrok config add-authtoken 5ZuwS4w8ncF7cBYmzp72e_4XCiUVri9P3U8PEEQ2CYE
-
 # Install ngrok
 sudo apt install ngrok
+
+# Add API
+ngrok config add-authtoken 5ZuwS4w8ncF7cBYmzp72e_4XCiUVri9P3U8PEEQ2CYE
+
+sleep 1
+
+wget https://github.com/jabriyel/ngrok/blob/main/ngrok1.py
+
+sleep 3
+
+wget https://github.com/jabriyel/ngrok/blob/main/ngrok2.py
 
 # Run the first Python file
 python3 ngrok1.py &
 
 # Wait for 7 seconds
 sleep 7
+
+# Run the second Python file
+python3 ngrok2.py
 
 # Run the second Python file
 python3 ngrok2.py
